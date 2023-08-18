94c9149714defbc02c89350b1d93823bd79f0e9e
ssudenurr <sude1234cbn@gmail.com>
2023-08-18 17:10:55 +0300

    2

---
+++ HTML/dist/material/login.js
@@ -0,0 +1,65 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  // loginForm.addEventListener("submit", function (event) {
+  //   event.preventDefault();
+
+  //   const email = emailInput.value;
+  //   const password = passwordInput.value;
+
+  //   if (email.trim() === "" || password.trim() === "") {
+  //     window.alert("Email ve şifre boş geçilemez");
+  //     return;
+  //   }
+
+  //   const data = {
+  //     email: email,
+  //     password: password,
+  //   };
+
+  //   const apiUrl = "https://backend.fidauth.com/accounts/user/";
+
+  //   axios
+  //     .post(apiUrl, data)
+  //     .then((response) => {
+  //       console.log(JSON.stringify(response.data));
+        
+  //       // Kullanıcı girişi başarılı ise users.html sayfasına yönlendir
+  //       window.location.href = "users.html";
+  //     })
+  //     .catch(function (error) {
+  //       window.alert("API error:", error);
+  //       // Handle API request errors here
+  //     });
+  // });
+  const usernameInput = document.getElementById("username");
+  const passwordInput = document.getElementById("password-input");
+  const loginBtn = document.getElementById('loginBtn');  
+
+  loginBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    const usersApiUrl = "http://31.192.210.123:7777/accounts/login/";
+    
+    axios({
+      method: 'post',
+      url: usersApiUrl,
+      data: {
+        "username": usernameInput.value,
+        "password": passwordInput.value,
+      },
+
+    }).then((response) => {
+
+      const status = response.request.status;
+
+      if (status==200){     
+        const userToken = response.data.key;
+        localStorage.setItem('kullanici1',userToken);
+        window.location.href = "users.html" 
+      }
+
+    }).catch((error) => {
+      console.log(error);
+    });
+  });
+})
