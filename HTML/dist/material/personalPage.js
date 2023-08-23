f6df2e460801e93f421222b9d30585ec8a55c1d2
ssudenurr <sude1234cbn@gmail.com>
2023-08-23 12:54:24 +0300

    purchase table edit

--- HTML/dist/material/personalPage.js
+++ HTML/dist/material/personalPage.js
@@ -45,5 +45,3 @@ addBtn.addEventListener('click', function() {
 
 
 
-
-
---
+++ HTML/dist/material/purchaseScreen.js
@@ -0,0 +1,46 @@
+const addBtn = document.getElementById('addBtn')
+const deleteBtn = document.getElementById('deleteBtn')
+
+
+const productName = document.getElementById('productName');
+const price = document.getElementById('price');
+const link = document.getElementById('link');
+const description = document.getElementById('description');
+
+
+function addRowTable(user,productName,price,link,description){
+    const tableBody = document.querySelector('#purchaseTable tbody')
+
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+    <td><input class="form-check-input" type="checkbox" value=""></td>
+    <td>${user}</td>
+    <td>${productName}</td>
+    <td>${price}</td>
+    <td>${link}</td>
+    <td>${description}</td>
+    `;
+    tableBody.appendChild(newRow)
+}
+addBtn.addEventListener('click', function() {
+    const user = "A kişisi "
+    const productValue = productName.value;
+    const priceValue = price.value;
+    const linkValue = link.value;
+    const descriptionValue = description.value;
+
+    addRowTable(user,productValue, priceValue, linkValue, descriptionValue,);
+
+    productName.value = '';
+    price.value = '';
+    link.value = '';
+    description.value = '';
+
+});
+
+
+
+
+
+
+
