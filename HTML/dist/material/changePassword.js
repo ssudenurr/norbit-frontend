e68b7a1bfd45bbc2624405a6ff61097df3fa1319
ssudenurr <sude1234cbn@gmail.com>
2023-08-25 11:01:10 +0300

    change password code

--- HTML/dist/material/changePassword.js
+++ HTML/dist/material/changePassword.js
@@ -1,1 +1,30 @@
 
+const changePassword = document.getElementById('changePassword')
+
+changePassword.addEventListener('click',function(e){
+    e.preventDefault();
+    const username = document.getElementById('username')
+    const password = document.getElementById("password-input");
+
+    const apiUrl = "http://backend.norbit.com.tr/accounts/password/change/";
+    const token = localStorage.getItem('token');
+
+    const data = {
+        new_password: password
+    };
+
+    axios({
+        method:'patch',
+        url:apiUrl,
+        data:data,
+        headers: {
+            "Authorization": `Token ${token}`
+        },
+    }) .then(response => {
+        console.log('Şifre değiştirildi', response.data);
+
+    }).catch(error => {
+        console.error('Hata oluştu', error);
+    });
+
+})
