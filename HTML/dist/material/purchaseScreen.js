ea2e48085b48c77fff54941aed031f848c7cbf5e
ssudenurr <sude1234cbn@gmail.com>
2023-09-01 13:03:00 +0300

    edit

--- HTML/dist/material/purchaseScreen.js
+++ HTML/dist/material/purchaseScreen.js
@@ -39,7 +39,7 @@ addBtn.addEventListener('click', function() {
     const descriptionValue = description.value;
     const purchasingDate = "2022-07-11T09:40:00Z"
 
-    const apiUrl= "http://backend.norbit.com.tr/purchase/list/"
+    const apiUrl= "http://backend.norbit.com.tr/purchase-request/list/"
     const token  = localStorage.getItem('token');
     axios ({
         method:'post',
@@ -91,67 +91,67 @@ function clearInput(){
 
 
 
-function addPurchaseValue() {
-    const apiUrl = "http://backend.norbit.com.tr/purchase-request/list/";
-    const token  = localStorage.getItem('token');
+// function addPurchaseValue() {
+//     const apiUrl = "http://backend.norbit.com.tr/purchase-request/list/";
+//     const token  = localStorage.getItem('token');
 
-    axios({
-        method: 'get',
-        url: apiUrl,
-        headers: {
-            "Authorization": `Token ${token}`
-        },
-    })
-    .then(response => {
-        const purchaseData = response.data;
-        console.log(purchaseData); 
-        populateTable(purchaseData.results); 
-    })
-    .catch(error => {
-        console.error('Hata oluştu', error);
-    });
-}
+//     axios({
+//         method: 'get',
+//         url: apiUrl,
+//         headers: {
+//             "Authorization": `Token ${token}`
+//         },
+//     })
+//     .then(response => {
+//         const purchaseData = response.data;
+//         console.log(purchaseData); 
+//         populateTable(purchaseData.results); 
+//     })
+//     .catch(error => {
+//         console.error('Hata oluştu', error);
+//     });
+// }
 
-function populateTable(purchaseData) {
-    const tableBody = document.querySelector('#purchaseTable tbody')
-    tableBody.innerHTML = ''; 
+// function populateTable(purchaseData) {
+//     const tableBody = document.querySelector('#purchaseTable tbody')
+//     tableBody.innerHTML = ''; 
 
-    purchaseData.forEach(item => {
-        const newRow = document.createElement('tr');
-        newRow.innerHTML = `
-            <td><input class="form-check-input" type="checkbox" value=""></td>
-            <td>${item.personName}</td>
-            <td>${item.product_name}</td>
-            <td>${item.price}</td>
-            <td>${item.e_commerce_site}</td>
-            <td>${item.description}</td>
-            <td>${item.purchasing_date}</td>
-            <td><button id="editBtn" class="btn btn-success btn-sm edit-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button></td>
-            <td><button id="deleteBtn" class="btn btn-danger btn-sm delete-btn"  data-bs-toggle="modal"   >Delete</button></td>
-        `;
+//     purchaseData.forEach(item => {
+//         const newRow = document.createElement('tr');
+//         newRow.innerHTML = `
+//             <td><input class="form-check-input" type="checkbox" value=""></td>
+//             <td>${item.personName}</td>
+//             <td>${item.product_name}</td>
+//             <td>${item.price}</td>
+//             <td>${item.e_commerce_site}</td>
+//             <td>${item.description}</td>
+//             <td>${item.purchasing_date}</td>
+//             <td><button id="editBtn" class="btn btn-success btn-sm edit-btn" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button></td>
+//             <td><button id="deleteBtn" class="btn btn-danger btn-sm delete-btn"  data-bs-toggle="modal"   >Delete</button></td>
+//         `;
 
-        tableBody.appendChild(newRow);
+//         tableBody.appendChild(newRow);
 
-        const editBtns = document.querySelectorAll('.edit-btn'); 
-        editBtns.forEach(editBtn => {
-            editBtn.addEventListener('click', () => {    
-                modalContent.style.display = "block";
+//         const editBtns = document.querySelectorAll('.edit-btn'); 
+//         editBtns.forEach(editBtn => {
+//             editBtn.addEventListener('click', () => {    
+//                 modalContent.style.display = "block";
               
-                const personNameValue = row.querySelector('.person-name').textContent;
-                const productNameValue = row.querySelector('.product-name').textContent;
-                const priceValue = row.querySelector('.price').textContent;
-                const linkValue = row.querySelector('.link').textContent;
-                const descriptionValue = row.querySelector('.description').textContent;
+//                 const personNameValue = row.querySelector('.person-name').textContent;
+//                 const productNameValue = row.querySelector('.product-name').textContent;
+//                 const priceValue = row.querySelector('.price').textContent;
+//                 const linkValue = row.querySelector('.link').textContent;
+//                 const descriptionValue = row.querySelector('.description').textContent;
                 
-                openEditModal(personNameValue, productNameValue, priceValue, linkValue, descriptionValue);
-            });
-        });
-    });
-}
+//                 openEditModal(personNameValue, productNameValue, priceValue, linkValue, descriptionValue);
+//             });
+//         });
+//     });
+// }
 
-window.onload = function () {
-    addPurchaseValue();
-}
+// window.onload = function () {
+//     addPurchaseValue();
+// }
 
 
-personName.value = newRow.querySelector()
+// personName.value = newRow.querySelector()
