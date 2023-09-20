39e6afc89db6bd355018efc12b0e8fd93525d69e
ssudenurr <sude1234cbn@gmail.com>
2023-09-20 11:15:38 +0300

    edit to company Name value

--- HTML/dist/material/profilePage.js
+++ HTML/dist/material/profilePage.js
@@ -17,17 +17,75 @@ axios ({
       console.log(error);
     });
 }
-function userDetails(userData){
+async function userDetails(userData){
+
+    const company = await getCompanyNameId(userData.company_name);
+    
     document.getElementById('userNameValue').textContent = userData.username;
     document.getElementById("firstNameValue").textContent = userData.first_name;
     document.getElementById("lastNameValue").textContent = userData.last_name;
     document.getElementById("lastLoginValue").textContent = userData.last_login;
     document.getElementById("emailValue").textContent = userData.email;
-    document.getElementById("companyNameValue").textContent = userData.company_name;
+    document.getElementById("companyNameValue").textContent = company;
     document.getElementById("userTypeValue").textContent = userData.user_type;
+
 }
+function getCompanyName() {      // GET COMPANY NAME
+    const apiUrl= "http://backend.norbit.com.tr/company/list/"
+    const token  = localStorage.getItem('token');
+
+    axios({
+        method:'get',
+        url:apiUrl,
+        headers:{ 
+            "Authorization": `Token ${token}`
+        },
+    }).then((response)=>{
+        const companyData = response.data.results;
+
+        const companyList = document.getElementById('inputCompany')
+        companyList.innerHTML = '';
+
+        companyData.forEach((company) => {
+            const companyName = document.getElementById("companyNameValue");
+            companyName.text = company.company_name;
+
+        });
+
+    }).catch((error) => {
+          console.log(error);
+        });
+}
+const getCompanyNameId  = async (id) => {    // GET COMPANY NAME ID
+    const apiUrl= `http://backend.norbit.com.tr/company/${id}/`
+    const token  = localStorage.getItem('token');
+
+    const api = new Promise((resolve, reject) => {
+        axios({
+            method:'get',
+            url:apiUrl,
+            headers:{ 
+                "Authorization": `Token ${token}`
+            },
+        }).then((response)=>{
+            const companyList = response.data.company_name;
+            resolve(companyList)
+        }).catch((error) => {
+            reject("null")
+        });
+    });
+
+    try {
+        const response = await api;
+        return response;
+    }
+    catch (e) {
+        return e
+    }
+};
 
 window.onload = function () {
     getUserInfo();
-
+    getCompanyName();  
+    getCompanyNameId();
 };
