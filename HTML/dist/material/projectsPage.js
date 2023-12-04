dee919ff0284df343532b0dfd2a46910024192f2
ssudenurr <sude1234cbn@gmail.com>
2023-12-04 16:43:51 +0300

    taken employees data and loginned user value

--- HTML/dist/material/projectsPage.js
+++ HTML/dist/material/projectsPage.js
@@ -11,11 +11,28 @@ const projectCompany = document.getElementById('company');
 const employeesOption = document.getElementById('employees')
 
 const createButton = document.getElementById('createButton');
+createButton.addEventListener('click', () =>{
 
+    clearInput();
+    saveButton.style.display = "none";
+    addBtn.style.display = "block";
+
+});
+
+function clearInput(){
+projectName.value = "";
+projectDescription.value = "";
+projectstartDate.value = "";
+projectEndDate.value = "";
+projectCustomer.value = "";
+projectCompany.value = "";
+employeesOption.value = "";
+
+}
 const saveButton = document.getElementById('save-btn');
 addBtn.addEventListener('click', ()=>{
         addNewProject();
-        // window.location.reload();    
+        window.location.reload();
 
 })
 
@@ -27,19 +44,19 @@ function valueControl() {
       !projectstartDate.value ||
       !projectCustomer.value ||
       !projectCompany.value ||
-      !employeesOption.value 
+      !employeesOption.value
     ) {
       alert.style.display = "block";
-  
+
       setTimeout(() => {
         alert.style.display = "none";
       }, 1600);
-  
+
       return;
     }
     alert.style.display = "none";
   }
-  
+
   function formatTarih(tarih) {
     if (tarih) {
       const tarihParcalari = tarih.split("T");
@@ -83,10 +100,10 @@ function formatDateToCustomFormat(date) {
     var dd = String(date2.getDate()).padStart(2, "0"); // Günü 2 basamaklı hale getiriyoruz
     var hh = String(date2.getHours()).padStart(2, "0"); // Saati 2 basamaklı hale getiriyoruz
     var mm = String(date2.getMinutes()).padStart(2, "0"); // Dakikayı 2 basamaklı hale getiriyoruz
-  
+
     // Sonuç formatını birleştiriyoruz
     var formattedDate = yyyy + "-" + MM + "-" + dd + "T" + hh + ":" + mm;
-  
+
     return formattedDate;
   }
 function getProjectsInfo(page){
@@ -117,28 +134,29 @@ async function getProjectsDetails(pageId){
         headers: {
             "Authorization": `Token ${token}`
         },
-        
+
     }).then( async (response) =>{
         const projectData = response.data;
-        console.log(projectData)
-        const employeesDataId = await getEmployeesId(
-            projectData.employees
-        )
-        const nameData = projectData.project_name || '-';     
+
+        let employeesDataArray= [];
+        const employeesValues = projectData.employess.map(employeeId => getEmployeesId(employeeId)); // gelen dizideki değerleri employeeId değerini veriyor her değeri tek tek getEmployeesId fonksiyonuna atıyor
+        employeesDataArray = employeesDataArray.concat(employeesValues);
+
+        const nameData = projectData.project_name || '-';
         const descriptionData = projectData.description || '-';
         const startDateData = projectData.project_start_date;
         const endData = projectData.project_end_date;
         const customerData = projectData.customer || '-';
         const companyData = projectData.company || '-';
 
-        employees.value = employeesDataId;
+        employeesOption.value = employeesDataArray;
         projectName.value = nameData;
         projectDescription.value = descriptionData;
         projectstartDate.value = formatTarih(startDateData);
         projectEndDate.value = formatTarih(endData);
         projectCustomer.value = customerData;
         projectCompany.value = companyData;
-        employeesData(projectData.employees);
+        employeesData(projectData.employess);
 
 
     }).catch((error) =>{
@@ -149,69 +167,62 @@ async function getProjectsDetails(pageId){
 async function projectDetails(projectData) {
     projectList.innerHTML = ''; // Projeleri temizle
 
-    for ( const project of projectData) {
+    const loginnedUser = await getUserInfoId();
+
+
+    for (const project of projectData) {
         const projectDiv = document.createElement('div');
         projectDiv.classList.add('card', 'mb-3');
         const company = await getCompanyNameId(project.company) || '-';
         const employeesIds = project.employess;
         const employeesValue = await getEmployeesData(employeesIds);
         const users = employeesValue.map(user => `${user.first_name} ${user.last_name}`);
-        if(project.status === "FNS"){
+        if (project.status === "FNS") {
             project.status = "TAMAMLANDI"
-        }else{
+        } else {
             project.status = "DEVAM EDİYOR"
         }
         projectDiv.innerHTML = `
         <div class="card-body">
             <div class="text-muted">
-                <div class ="row">
-
+                <div class="row">
                     <div class="col-lg-5 col-sm-4">
                         <p class="mb-2 fw-bold fs-14 text-dark">PROJE ADI:</p>
                         <p class="mb-3 ">${project.project_name || '-'}</p>
                     </div>
-
                     <div class="col-lg-7 col-sm-10">
                         <p class="mb-2 fw-bold fs-14 text-dark">PROJEDE YER ALANLAR:</p>
                         <p class="mb-3 ">${users.join(', ') || '-'}</p>
                     </div>
-
                 </div>
-
                 <p class="mb-2 fw-bold fs-14 text-dark">AÇIKLAMA :</p>
                 <p>${project.description || '-'}</p>
-
                 <div class="pt-3 border-top border-top-dashed mt-4">
                     <div class="row">
-    
                         <div class="col-lg-2 col-sm-4">
                             <div>
-                                <p class="mb-2 fw-bold fs-14 text-dark" text-dark>BAŞLANGIÇ TARİHİ :</p>
+                                <p class="mb-2 fw-bold fs-14 text-dark">BAŞLANGIÇ TARİHİ :</p>
                                 <p class="fs-15 mb-0">${formatTarih(project.project_start_date) || '-'}</p>
                             </div>
                         </div>
-    
                         <div class="col-lg-2 col-sm-4">
                             <div>
                                 <p class="mb-2 fw-bold fs-14 text-dark">BİTİŞ TARİHİ :</p>
                                 <p class="fs-15 mb-0">${project.project_end_date ? formatTarih(project.project_end_date) : '-'}</p>
                             </div>
                         </div>
-    
                         <div class="col-lg-2 col-sm-4">
                             <div>
                                 <p class="mb-2 fw-bold fs-14 text-dark">MÜŞTERİ :</p>
                                 <p class="fs-15 mb-0">${project.customer || '-'}</p>
                             </div>
                         </div>
-    
                         <div class="col-lg-2 col-sm-4">
                             <div>
                                 <p class="mb-2 fw-bold fs-14 text-dark ">DURUM :</p>
                                 <div class="badge bg-warning fs-12">${project.status || '-'}</div>
                             </div>
                         </div>
-    
                         <div class="col-lg-2 col-sm-4">
                             <div>
                                 <p class="mb-2 fw-bold fs-14 text-dark">ŞİRKET :</p>
@@ -219,53 +230,78 @@ async function projectDetails(projectData) {
                             </div>
                         </div>
                         <div class="mt-auto d-flex justify-content-end align-items-end">
-                        <div>
-                            <button id="editBtn" class="btn btn-outline-success fs-14 btn-m fw-semibold edit-btn" style="letter-spacing: 0.3px; "onclick='createSaveButton(${project.id})' data-bs-toggle="modal" data-bs-target="#exampleModal">Düzenle</button>
+                            <div>
+
+                                ${loginnedUser === "AdminUser" ? `<button id="editBtn" class="btn btn-outline-success fs-14 btn-m fw-semibold edit-btn" style="letter-spacing: 0.3px;"
+                                onclick='createSaveButton(${project.id})' data-bs-toggle="modal" data-bs-target="#exampleModal">Düzenle</button>` : ''}
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
             </div>
         </div>
     `;
-    
 
         projectList.appendChild(projectDiv);
-    };
-}
+    }
+};
 
-async function createSaveButton(pageId) {
+
+async function getUserInfoId (){
+    try {
+      const apiUrl = `${baseUrl}accounts/user/`;
+      const token = localStorage.getItem("token");
+
+      const response = await axios({
+        method: "get",
+        url: apiUrl,
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      });
+
+      const userInfo = response.data.user_type;
+      console.log(userInfo);
+      if (userInfo === "AdminUser") {
+        createButton.style.display = "block"
+      }
+
+      return userInfo;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  async function createSaveButton(pageId) {
     document.querySelector('.modal-title').innerHTML = '';
 
     saveButton.style.display = 'block';
     addBtn.style.display = 'none';
 
     await getProjectsDetails(pageId);
-    
-    // Get the selected employees' IDs and pass them to employeesData
-    // const selectedEmployeeIds = Array.from(employeesOption.selectedOptions).map(option => option.value);
-    employeesData();
 
     saveButton.addEventListener('click', async () => {
-        await editToProject(pageId);
+        // Get the selected employees' IDs and pass them to editToProject
+        const selectedEmployees = Array.from(employeesOption.selectedOptions).map(option => option.value);
+        console.log(selectedEmployees);
+        await editToProject(pageId, selectedEmployees);
         // modal.hide();
-        // window.location.reload();
+        window.location.reload();
     });
 }
 
-
-const editToProject = async (itemId) => {
+const editToProject = async (itemId, selectedEmployees) => {
     const pageApi = `${baseUrl}projects/${itemId}/`;
     const token = localStorage.getItem('token');
-    const newEmployees = Array.from(document.getElementById('employees').selectedOptions).map(option => option.value);
-
+    console.log(selectedEmployees);
     const data = {
         project_name: projectName.value,
         description: projectDescription.value,
         project_start_date: formatDateToCustomFormat(projectstartDate.value),
         customer: projectCustomer.value,
         company: projectCompany.value,
-        employees: newEmployees,
+        employess: selectedEmployees,  // Pass selected employees here
     };
 
     if (projectEndDate.value) {
@@ -282,7 +318,7 @@ const editToProject = async (itemId) => {
     }).then((response) => {
         const projectData = response.data;
         console.log(projectData);
-        window.location.reload();
+        // window.location.reload();
     }).catch((error) => {
         console.error(error);
     });
@@ -297,7 +333,7 @@ const getCompanyNameId  = async (id) => {    // GET COMPANY NAME ID
         axios({
             method:'get',
             url:apiUrl,
-            headers:{ 
+            headers:{
                 "Authorization": `Token ${token}`
             },
         }).then((response)=>{
@@ -350,9 +386,9 @@ function getCompanyName() {
 //     let pageNumber = 1;
 
 //     const token = localStorage.getItem("token");
-  
 
-  
+
+
 //     while (true) {
 //         const apiUrl = `http://backend.norbit.com.tr/ems/list/?page=${pageNumber}`;
 //       const response = await axios({
@@ -362,19 +398,19 @@ function getCompanyName() {
 //           Authorization: `Token ${token}`,
 //         },
 //       });
-  
+
 //       const pageData = response.data.results;
 //       allPersonNameData = allPersonNameData.concat(pageData);
-  
+
 //       if (!response.data.next) {
 //         break; // Son sayfa kontrolü
 //       }
-  
+
 //       pageNumber++;
 //     }
-  
+
 //     employeesOption.innerHTML = "";
-  
+
 //     allPersonNameData.forEach((person) => {
 //       const option = document.createElement("option");
 //       option.value = person.id;
@@ -385,12 +421,12 @@ function getCompanyName() {
 //       employeesOption.appendChild(option);
 //     });
 //   }
-  
+
 
 async function employeesData(selectedEmployeeIds, allPersonNameData = [], pageNumber = 1) {
     const apiUrl = `${baseUrl}ems/list/?page=${pageNumber}`;
     const token = localStorage.getItem("token");
-  
+
     try {
       const response = await axios({
         method: "get",
@@ -399,14 +435,15 @@ async function employeesData(selectedEmployeeIds, allPersonNameData = [], pageNu
           Authorization: `Token ${token}`,
         },
       });
-  
+
       const pageData = response.data.results;
       allPersonNameData = allPersonNameData.concat(pageData);
-  
+
       employeesOption.innerHTML = "";
-  
+
       allPersonNameData.forEach((person) => {
         const option = document.createElement("option");
+
         option.value = person.id;
         option.text = person.username;
         if (selectedEmployeeIds && selectedEmployeeIds.includes(person.id)) {
@@ -414,7 +451,8 @@ async function employeesData(selectedEmployeeIds, allPersonNameData = [], pageNu
         }
         employeesOption.appendChild(option);
       });
-  
+
+
       const nextPage = response.data.next;
       if (nextPage) {
         await employeesData(selectedEmployeeIds, allPersonNameData, pageNumber + 1);
@@ -424,8 +462,8 @@ async function employeesData(selectedEmployeeIds, allPersonNameData = [], pageNu
     }
   }
 
-  
-  
+
+
 const getEmployeesId = async (id) => {
 
 const apiUrl = `${baseUrl}ems/employee/${id}`;
@@ -476,7 +514,7 @@ async function getEmployeesData(employeeIds) {
 }
 
 const addNewProject = async () =>{
-    const apiUrl = `${baseUrl}projects/create/`; 
+    const apiUrl = `${baseUrl}projects/create/`;
     const token  = localStorage.getItem('token');
     valueControl()
 
@@ -488,7 +526,7 @@ const addNewProject = async () =>{
     const projectCustomer = document.getElementById('customer').value;
     const projectCompany = document.getElementById('company').value;
 
-    const selectedEmployees = Array.from(document.getElementById('employees').selectedOptions).map(option => option.value);
+    const selectedEmployees = Array.from(employeesOption.selectedOptions).map(option => option.value);
     const data = {
         project_name: projectName,
         description: projectDescription,
@@ -501,10 +539,10 @@ const addNewProject = async () =>{
     if (projectEndDate) {
         data.project_end_date = formatDateToCustomFormat(projectEndDate);
     }
-    axios({        
+    axios({
         method:'post',
         url:apiUrl,
-        headers:{ 
+        headers:{
             "Authorization": `Token ${token}`
         },
         data:data,
@@ -523,6 +561,7 @@ window.onload = function () {
     getProjectsInfo(1);
     getCompanyName();
     employeesData();
+    getUserInfoId ()
 }
 
 
