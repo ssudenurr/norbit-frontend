735be29b7d5ce2dea0116315bb46a81d272c53c6
ssudenurr <sude1234cbn@gmail.com>
2024-01-23 11:12:57 +0300

    sorted table data, corrected date formats , commend line added

--- HTML/dist/material/purchasePage.js
+++ HTML/dist/material/purchasePage.js
@@ -17,21 +17,25 @@ const linkInput = document.getElementById('inputLink');
 const purchasingDateInput = document.getElementById('inputPurchasingDate');
 const descriptionInput = document.getElementById('inputDescription');
 const orderDateInput = document.getElementById('inputOrderDate');
-const ownerNameInput = document.getElementById('inputOwnerName');
+const orderingPersonInput = document.getElementById('inputOrderingPerson');
 const rowEdit = document.getElementById('editBtn');
 
-function formatTarih(tarih) {
-    if (!tarih) {
-      return ""; // or handle the null case as needed
+/*UZUN FORMATTAKİ TARİHİ KISA FORMATA ÇEVİRİR */
+function formatTarih(date) {
+    if (date) {
+      const datePieces = date.split("T");
+      if (datePieces.length > 0) {
+        return datePieces[0];
+      }
     }
-    const tarihParcalari = tarih.split('T');
-    return tarihParcalari[0];
+    return null; 
   }
-  
-  function formatDateToCustomFormat(date) {
+/*KISA FORMATTAKİ TARİHİ UZUN FORMATA ÇEVİRİR */
+function formatDateToCustomFormat(date) {
     if (!date) {
         return null; // Handle the case where date is empty
     }
+
     let date2 = new Date(date);
     var yyyy = date2.getFullYear();
     var MM = String(date2.getMonth() + 1).padStart(2, '0');
@@ -39,14 +43,14 @@ function formatTarih(tarih) {
     var hh = String(date2.getHours()).padStart(2, '0');
     var mm = String(date2.getMinutes()).padStart(2, '0');
     var ss = String(date2.getSeconds()).padStart(2, '0');
-  
+
     // Format the date string
     var formattedDate = yyyy + "-" + MM + "-" + dd + "T" + hh + ":" + mm;
-  
+
     return formattedDate;
 }
-
-function getRowData(rowId){
+/*İLGİLİ SATIRIN BİLGİLERİNİ AÇILAN MODALDA GÖSTERİR */
+function getRowData(rowId){ 
     const apiUrl = `${baseUrl}purchase/${rowId}/`
     const token  = localStorage.getItem('token');
 
@@ -59,7 +63,7 @@ function getRowData(rowId){
     }).then( async (response)=>{
         const rowData = response.data;
         console.log(rowData);
-        // const ownerData = await getResponsibleId(rowData.owner);
+        const orderingPersonData = await getResponsibleId(rowData.ordering_person);
         
         const whereInTheOfficeInputData = rowData.where_in_the_office;
         const productNameData = rowData.product_name;
@@ -74,9 +78,15 @@ function getRowData(rowId){
         if (rowData.where_in_the_office) {
             whereInTheOfficeInput.value = rowData.where_in_the_office;
         } else {
-            whereInTheOfficeInput.value = ''; // Set an empty value
+            whereInTheOfficeInput.value = ''; 
         }
-        // ownerNameInput.value = ownerData;
+        orderingPersonInput.value = orderingPersonData;
+
+        if (rowData.ordering_person){
+            orderingPersonInput.value = rowData.ordering_person;
+        }else{
+            productNameInput.value = '';
+        }
         productNameInput.value = productNameData;
         priceInput.value = priceData;
         countInput.value = countData;
@@ -88,37 +98,44 @@ function getRowData(rowId){
         // descriptionInput.value = descriptionData;
 
     })
-}
+};
+
+/*SEÇİLEN PURCHASE DEĞERİNİ DÜZENLER*/
 function editPurchase(purchaseId) {
     const apiUrl = `${baseUrl}purchase/${purchaseId}/`;
     const token = localStorage.getItem('token');
+
+    const requestData = {
+        where_in_the_office: whereInTheOfficeInput.value,
+        product_name: productNameInput.value,
+        price: priceInput.value,
+        count: countInput.value,
+        e_commerce_site: linkInput.value,
+        created_at: formatDateToCustomFormat(purchasingDateInput.value),
+        siparis_verilen_tarih: formatDateToCustomFormat(orderDateInput.value),
+    };
+
+    // ordering_person değeri boşta gidebilir
+    if (orderingPersonInput.value.trim() !== '') {
+        requestData.ordering_person = [orderingPersonInput.value];
+    }
 
     axios({
         method: 'patch',
         url: apiUrl,
         headers: {
-            "Authorization": `Token ${token}`
+            'Authorization': `Token ${token}`
         },
-        data: {
-            where_in_the_office: whereInTheOfficeInput.value,
-            // owner: ownerNameInput.value,
-            product_name: productNameInput.value,
-            price: priceInput.value,
-            count: countInput.value,
-            e_commerce_site: linkInput.value,
-            created_at: formatDateToCustomFormat(purchasingDateInput.value),
-            siparis_verilen_tarih: formatDateToCustomFormat(orderDateInput.value), // Use empty string if it's empty
-            // description: newDescription.value,
-        }
+        data: requestData
     }).then((response) => {
-        // const userData = response.data
         window.location.reload();
-
+        // Handle the response as needed
     }).catch((error) => {
         console.error(error);
     });
 }
 
+/*SEÇİLEN PURCHASE DEĞERİNİ STATÜ DURUMUNU "İPTAL EDİLDİ" YAPAR */
 async function cancelRequest(requestId){
     const apiUrl = `${baseUrl}purchase-request/${requestId}/`;
     const token = localStorage.getItem('token');
@@ -148,7 +165,8 @@ async function cancelRequest(requestId){
         console.error('An error occurred while updating the status:', error);
 
     })
-}
+};
+/* SEÇİLEN PURCHASE DEĞERİNİ STATÜ DURUMUNU "TAMAMLANDI" YAPAR*/
 async function updatePurchaseStatus(requestId){
     const apiUrl = `${baseUrl}purchase/${requestId}/`;
     const token = localStorage.getItem('token');
@@ -178,7 +196,9 @@ async function updatePurchaseStatus(requestId){
         console.error('An error occurred while updating the status:', error);
 
     })
-}
+};
+
+/*SEÇİLEN PURCHASE DEĞERİNİ STATÜ DURUMUNU "BEKLENİYOR" YAPAR */
 async function replyRequest(requestId){
     const apiUrl = `${baseUrl}purchase-request/${requestId}/`;
     const token = localStorage.getItem('token');
@@ -190,14 +210,17 @@ async function replyRequest(requestId){
             "Authorization": `Token ${token}`
         },
          data:{
+            ordering_person: [],
+            siparis_verilen_tarih:null,
             status: 'BE',
         }
 
     }).then((response)=>{
         console.log(response.data)
-        if (response.status === 200) {
+        if (response.status === 200) {            
             console.log('Status updated successfully:', response.data);
-            window.location.reload();
+
+            // window.location.reload();
 
         } else {
             console.error('Status update failed:', response);
@@ -216,7 +239,7 @@ let itemsPerPage = 5;
 
 const nextPageBtn = document.getElementById("next-page");
 const prevPageBtn = document.getElementById("prev-page");
-
+/*SAYFALAR ARASINDA GEZİNMEK İÇİNDİR */
 function displayDataOnPage(){
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
@@ -225,18 +248,21 @@ function displayDataOnPage(){
     tableBody.innerHTML = "";
     showPurchase(pageData); 
 
-}
+};
+/*BİR SONRAKİ SAYFAYA GEÇMEK İÇİN */
 nextPageBtn.addEventListener('click', () => {
     currentPage++;
     getPurchase(currentPage);
 });
-
+/*BİR ÖNCEKİ SAYFAYA GEÇMEK İÇİN */
 prevPageBtn.addEventListener('click', () => {
     if (currentPage > 1) {
         currentPage--;
         getPurchase(currentPage);
     }
 });
+
+/*PURCHASE DEĞERLERİNİ ALIR */
 async function getPurchase(page = 1){
     const apiUrl = `${baseUrl}purchase/list/?page=${page}`;
     const token  = localStorage.getItem('token');
@@ -250,7 +276,7 @@ async function getPurchase(page = 1){
     }).then((response) =>{
         const responseData = response.data.results;
         const nextPage = response.next;
-        // getResponsiblePerson();
+        getOrderingPerson();
         showPurchase(responseData);
         responseData.map((item) => {
             dataList.push(item);
@@ -258,31 +284,78 @@ async function getPurchase(page = 1){
         if (nextPage) {
             getPurchase(nextPage);
         }
-        // console.log(responseData)
+        console.log(responseData)
 
     }).catch((error) =>{
         console.log(error)
     })
 }
+
+let currentSortOrder = 'asc';
+const table = document.getElementById("purchase-table");
+/*TABLODAKİ VERİLERİ ASC VE DESC OLARAK SIRALAR */
+function sortTable(columnIndex) { 
+  const rows = Array.from(table.querySelectorAll("tbody tr"));
+
+  rows.sort((a, b) => {
+    const aValue = a.children[columnIndex].textContent.trim();
+    const bValue = b.children[columnIndex].textContent.trim();
+    const comparison = aValue.localeCompare(bValue, undefined, { numeric: true, sensitivity: 'base' });  
+  
+    return currentSortOrder === 'asc' ? comparison : -comparison;
+  });
+
+  // Clear the table body
+  table.querySelector("tbody").innerHTML = "";
+
+
+  rows.forEach(row => {
+    table.querySelector("tbody").appendChild(row);
+  });
+  currentSortOrder = currentSortOrder === 'asc' ? 'desc' : 'asc';
+}
+
+/*TABLODAKİ BAŞLIKLARA TIKLANDIĞI ZAMAN BAŞLIĞIN VE SIRALANAN VERİLERİN RENGİ DEĞİŞİR */
+const headers = document.querySelectorAll("thead th");
+headers.forEach((header, index) => {
+  let sort_asc = true;
+  header.addEventListener("click", () => {
+  const rows = Array.from(table.querySelectorAll("tbody tr"));
+
+    headers.forEach(header => header.classList.remove('active'))
+    header.classList.add('active');
+
+    document.querySelectorAll('td').forEach(td => td.classList.remove('active'));
+    rows.forEach(row => {
+      row.children[index].classList.add('active')
+    });
+
+    header.classList.toggle('asc', sort_asc);
+    sort_asc = header.classList.contains('asc') ? false : true;
+    sortTable(index);
+  });
+});
+
+/*PURCHASE DEĞERLERİNİ TABLODA GÖSTERİR */
 const showPurchase = async (responseData)  => {
     tableBody.innerHTML = '';
     for( const purchase of responseData){
     // console.log(responseData);
-        const newRow = document.createElement('tr') ;
-        const responsiblePerson = await getResponsibleId(purchase.responsible_person)  || '-';
-        const productName = purchase.product_name || '-';
-        const ownerName = await getResponsibleId(purchase.owner)  || '-';
-        const price = purchase.price || '-';
-        const count = purchase.count || '-';
-        const e_commerce_site = purchase.e_commerce_site || '-';
-        const purchasing_date = formatTarih(purchase.created_at);
-        const orderDate = purchase.siparis_verilen_tarih ? formatTarih(purchase.siparis_verilen_tarih) : '-';
-        const description = purchase.description || '-';
-        let statusData = ownerName || purchase.siparis_verilen_tarih ? "Tamamlandı" : (purchase.status === "ON" ? "Onaylandı" : "-");
+    const newRow = document.createElement('tr');
+    const responsiblePerson = await getResponsibleId(purchase.responsible_person) || '-';
+    const productName = purchase.product_name || '-';
+    const orderingPersonName = (purchase.ordering_person && purchase.ordering_person.length > 0) ? await getResponsibleId(purchase.ordering_person) : '-';
+    const price = purchase.price || '-';
+    const count = purchase.count || '-';
+    const e_commerce_site = purchase.e_commerce_site || '-';
+    const purchasing_date = formatTarih(purchase.created_at);
+    const orderDate = purchase.siparis_verilen_tarih ? formatTarih(purchase.siparis_verilen_tarih) : '-';
+    const description = purchase.description || '-';
+    let statusData = purchase.siparis_verilen_tarih ? "Tamamlandı" : (purchase.status === "ON" ? "Onaylandı" : "-");
         newRow.innerHTML = `
         <td><input class="form-check" type ="checkbox"  id="checkbox" value=""</td>
         <td>${responsiblePerson}</td>
-        <td>${ownerName}</td>
+        <td>${orderingPersonName}</td>
         <td><span class="badge badge bg-success fw-semibold fs-12">${statusData}</span></td>
         <td>${productName}</td>
         <td>${price}</td>
@@ -320,9 +393,10 @@ const showPurchase = async (responseData)  => {
                 replyRequest(purchaseId);
             });
             editBtn.addEventListener('click', () => {
-                // getResponsiblePerson();
                 getRowData(purchaseId);
                 modal.show();
+                checkBox.checked = false;
+                // getOrderingPerson();
                 // editPurchase();
 
 
@@ -332,91 +406,86 @@ const showPurchase = async (responseData)  => {
             })
         })
     };
-}
+};
 
-
+/*İSTEKTEN SORUMLU KİŞİNİN İSİM BİLGİLERİNİ ALMAK İÇİNDİR*/
 const getResponsibleId = async (id) => {
     const apiUrl= `${baseUrl}ems/list/?id=${id}`
     const token  = localStorage.getItem('token');
 
-    const api = new Promise((resolve, reject) => {
-        axios({
-            method:'get',
-            url:apiUrl,
-            headers:{ 
-                "Authorization": `Token ${token}`
-            },
-        }).then((response)=>{
-            const responseData = response.data.results
-
-            const ownerData = responseData.map((item) => {
-                const firstname = item.first_name;
-                const lastname = item.last_name;
-                return firstname + ' ' + lastname;
-            });
-
-            resolve(ownerData);
-        }).catch((error) => {
-            reject(error)
-        
-        });
-    });
-
     try {
-        const response = await api;
-        return response;
+        const response = await axios({
+            method: 'get',
+            url: apiUrl,
+            headers: {
+                'Authorization': `Token ${token}`
+            },
+        });
+
+        const responseData = response.data.results;
+
+        const personData = responseData.map((item) => {
+            const firstname = item.first_name;
+            const lastname = item.last_name;
+            return firstname + ' ' + lastname;
+        });
+
+        return personData;
+    } catch (error) {
+        console.error(error);
+        return  ;
     }
-    catch (e) {
-        return e
-    }
-} 
+};
+
+/*SİPARİŞ VEREN KİŞİNİN İSMİNİ ALMAK İÇİN PERSONEL LİSTESİ ALINDI */
 let personList = [];
-// async function getResponsiblePerson(purchaseId, page = 1) {
-//     const apiUrl = `${baseUrl}ems/list/?page=${page}`;
-//     const token = localStorage.getItem("token");
+async function getOrderingPerson(purchaseId, page = 1) {
+    const apiUrl = `${baseUrl}ems/list/?page=${page}`;
+    const token = localStorage.getItem("token");
   
-//     try {
-//       const response = await axios({
-//         method: "get",
-//         url: apiUrl,
-//         headers: {
-//           Authorization: `Token ${token}`,
-//         },
-//       });
+    try {
+      const response = await axios({
+        method: "get",
+        url: apiUrl,
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      });
   
-//       const personNameData = response.data.results;
-//       console.log(response.data);
+      const personNameData = response.data.results;
+      console.log(response.data);
   
-//       const personListElement = document.getElementById("inputOwnerName");
+      const personListElement = document.getElementById("inputOrderingPerson");
   
-//       personNameData.forEach((person) => {
-//         const option = document.createElement("option");
-//         option.value = person.id;
-//         option.text = person.first_name + " " + person.last_name;
-//         if (person.id === purchaseId) {
-//           option.selected = true; // Seçilen kişiyi seçili olarak işaretle
-//         }
-//         personListElement.appendChild(option);
-//         personList.push(person);
-//       });
+      personNameData.forEach((person) => {
+        const option = document.createElement("option");
+        option.value = person.id;
+        option.text = person.first_name + " " + person.last_name;
+        if (person.id === purchaseId) {
+          option.selected = true; // Seçilen kişiyi seçili olarak işaretle
+        }
+        personListElement.appendChild(option);
+        personList.push(person);
+      });
   
-//       // Check if there are more pages
-//       const nextPage = response.data.next;
-//       if (nextPage !== null) {
-//         const nextPage = page + 1;
-//         return await getResponsiblePerson(personList, nextPage);
-//       } else {
-//         return personList;
-//       }
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
+      // Check if there are more pages
+      const nextPage = response.data.next;
+      if (nextPage !== null) {
+        const nextPage = page + 1;
+        return await getOrderingPerson(personList, nextPage);
+      } else {
+        return personList;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+};
+
 searchButton.addEventListener('click', () => {
     const searchTerm = searchInput.value;
         searchData(searchTerm);
 });
-
+/*TABLODAKİ VERİLER ARASINDA ARAMA YAPMA */
 async function searchData(searchTerm){
     const apiUrl = `http://backend.norbit.com.tr/inventory/list/?search=${searchTerm}`;
     const token  = localStorage.getItem('token');
@@ -464,6 +533,7 @@ searchInput.addEventListener('input', () => {
     }
 });
 
+/*GİRİŞ YAPAN KULLANICININ BİLGİLERİNİ ALIR */
 async function getUserInfoId (){
     try {
       const apiUrl = `${baseUrl}accounts/user/`;
